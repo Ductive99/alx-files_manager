433880d568234ad8f8eb38743176728f5dfeb83e
El Houssain Souhail <sou.ductive@gmail.com>
2024-07-03 23:29:10 +0100

    Fixed styling

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const PORT = process.env.PORT ? process.env.PORT : 5000;
 server.use(express.json());
 server.use(router);
 
-server.listen(PORT, () =>
-  console.log(`Server Running On PORT: ${PORT}`)
-);
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
