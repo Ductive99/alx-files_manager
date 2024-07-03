9a7bbbe85e4a5f9de650f44dab82799bad9909c3
El Houssain Souhail <sou.ductive@gmail.com>
2024-07-03 23:02:44 +0100

    Added DBClient class

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
+    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
+    const database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
+    const DBurl = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(DBurl, { useUnifiedTopology: true });
+    this.isConnected = false;
+    this.client.connect()
+      .then(() => {
+        this.isConnected = true;
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async nbUsers() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('users').countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('files').countDocuments();
+    return users;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
